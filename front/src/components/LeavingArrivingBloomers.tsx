25e557c9ea8c6396c696c904b064de1a963e524c
Joris Mansion <juhh@juhh-core.lan>
2024-06-25 19:01:02 +0200

    formatting data

--- front/src/components/LeavingArrivingBloomers.tsx
+++ front/src/components/LeavingArrivingBloomers.tsx
@@ -1,13 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+type Freelance = {
+  firstname: string;
+  lastname: string;
+  email: string;
+};
+
+type Mission = {
+  id: number;
+  beginDate: string;
+  endDate: string;
+  missionType: string;
+  label: string;
+  freelance: Freelance;
+};
 
 export default function LeavingArrivingBloomers() {
-    
+  const [missions, setMissions] = useState([]);
+
+  const isDateIsWithinNextMonth = (date: Date) => {
+    const now = new Date();
+    const nextMonth = new Date(now.getFullYear(), now.getMonth() + 2, 0);
+
+    return date >= now && date <= nextMonth;
+  };
+
+  const nextBeginningMissions = missions.filter((mission: Mission) => {
+    return isDateIsWithinNextMonth(new Date(mission.beginDate));
+  });
+
+  const nextEndingMissions = missions.filter((mission: Mission) => {
+    return isDateIsWithinNextMonth(new Date(mission.endDate));
+  });
+
+  const arriving = new Map();
+  const leaving = new Map();
+
+  nextBeginningMissions.forEach((mission: Mission) => {
+    if (arriving.has(mission.beginDate)) {
+      arriving.get(mission.beginDate).push(mission);
+    } else {
+      arriving.set(mission.beginDate, [mission]);
+    }
+  });
+
+  nextEndingMissions.forEach((mission: Mission) => {
+    if (leaving.has(mission.endDate)) {
+      leaving.get(mission.endDate).push(mission);
+    } else {
+      leaving.set(mission.endDate, [mission]);
+    }
+  })
+
+  console.log(arriving, leaving);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:3000/missions");
         const data = await response.json();
-        console.log(data);
+        setMissions(data);
       } catch (error) {
         console.error(error);
       }
